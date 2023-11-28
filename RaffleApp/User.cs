8ce9630f785641a3912a0c615244121c9fd5081b
powertec3 <maharishitec@yahoo.com>
2023-11-28 23:48:24 +0800

    Add project files.

---
+++ RaffleApp/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RaffleApp
+{
+    public class User
+    {
+        private string _name { get; set; }
+        private int _nooftickets { get; set; }  //Total Tickets purchased
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public int NoOfTickets
+        {
+            get => _nooftickets;
+            set => _nooftickets = value;
+        }
+        public User(string name, int tickets)
+        {
+            Name = name;
+            NoOfTickets = tickets;
+        }
+
+
+    }
+}
